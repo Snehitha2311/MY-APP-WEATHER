6379d8e05dfb4673449efba6ed956dcf7516eef5
Snehitha2311 <69804874+Snehitha2311@users.noreply.github.com>
2021-06-30 12:38:31 +0530

    Add files via upload

---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/interior1.jfif'
+import ImageTwo from '../images/download4.jpg'
+// import ImageThree from '../images/house-3.jpg'
+// import ImageFour from '../images/house-4.jpg'
+
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse quaerat vel at officis,sit cumque, rerum, neque  perspiciatis odit reiciends natus alias voluptatum corrupti volutas fuga quod incidunt modi saepe.',
+    paragraphTwo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse quaerat vel at officis,sit cumque, rerum, neque  perspiciatis odit reiciends natus alias voluptatum corrupti volutas fuga quod incidunt modi saepe.',
+    paragraphTwo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+}
---
+++ src/data/SliderData.js
@@ -0,0 +1,68 @@
+import ImageOne from '../images/download.jfif'
+import ImageTwo from '../images/download1.jfif'
+import ImageThree from '../images/download2.jfif'
+import ImageFour from '../images/download3.jfif'
+
+
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonasia',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Casta Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: 'House in Las Vegas, Nevada',
+//         price: '$8,980,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageFive,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in Houston, Texas',
+//         price: '$3,460,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSix,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'House in Alajuela, Costa Rica',
+//         price: '$5,420,000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageSeven,
+//         alt: 'House'
+//     }
+// ];
